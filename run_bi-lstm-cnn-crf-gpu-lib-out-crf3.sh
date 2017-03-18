b34381b874e45925a9b02c6c5db058930032bf45
Kurt Espinosa <brevisiunius@gmail.com>
2017-03-18 20:42:33 +0000

    first commit

---
+++ run_bi-lstm-cnn-crf-gpu-lib-out-crf3.sh
@@ -0,0 +1,4 @@
+THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32,lib.cnmem=1 python bi_lstm_cnn_crf3.py --fine_tune --embedding glove --oov embedding --update momentum \
+ --batch_size 10 --num_units 200 --num_filters 30 --learning_rate 0.01 --decay_rate 0.05 --grad_clipping 5 --regular none --dropout \
+ --train "data/train" --dev "data/dev" --test "data/test" \
+ --embedding_dict "emb/twitter/glove.twitter.27B.100d.txt" --patience 5 --output_prediction
